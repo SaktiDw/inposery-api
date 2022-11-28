96cf200715c2e50add4ab7b1c9b563760b5eef51
Sapeiidw <sapeiidw@gmail.com>
2022-11-29 02:37:26 +0800

    index page

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return ['Laravel' => app()->version()];
+    return view("index", $data = ['Laravel' => app()->version()]);
 });
 
 // require __DIR__ . '/auth.php';
